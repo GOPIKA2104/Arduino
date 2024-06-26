1959494fb9739b3caf71a7a84d078c0648544339
GOPIKA2104 <gopss3108@gmail.com>
2024-06-26 11:36:26 +0530

    Add files via upload

---
+++ buzzer2freq.ino
@@ -0,0 +1,25 @@
+const int buzzer = 9;
+
+void setup()
+{
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop()
+{
+  
+  tone(buzzer, 500); // send 1khz sound signal to buzzer
+  delay(1000); // Wait for 1000 millisecond(s)
+  noTone(buzzer);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  tone(buzzer, 1000); // send 1khz sound signal to buzzer
+  delay(1000); // Wait for 1000 millisecond(s)
+  noTone(buzzer);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  tone(buzzer, 1500); // send 1khz sound signal to buzzer
+  delay(1000); // Wait for 1000 millisecond(s)
+  noTone(buzzer);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
